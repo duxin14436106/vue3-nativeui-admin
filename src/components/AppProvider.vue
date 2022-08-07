53b7a0e17145462f9b77c13a0a294cc43f888b6c
duxin14436106 <1284573827@qq.com>
2022-08-07 23:22:16 +0800

    初始化架构 仿配置

---
+++ src/components/AppProvider.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { useDialog, useLoadingBar, useMessage, useNotification } from 'naive-ui'
+
+// 挂载naive组件的方法至window, 以便在全局使用
+function setupNaiveTools() {
+  window.$loadingBar = useLoadingBar()
+  window.$message = useMessage()
+  window.$dialog = useDialog()
+  window.$notification = useNotification()
+  console.log(456)
+}
+
+const NaiveProviderContent = defineComponent({
+  setup() {
+    setupNaiveTools()
+  },
+  render() {
+    return h('div')
+  },
+})
+
+</script>
+
+<template>
+    <n-loading-bar-provider>
+      <n-dialog-provider>
+        <n-notification-provider>
+          <n-message-provider>
+            第一步框架
+<!--            <slot />-->
+            <NaiveProviderContent />
+          </n-message-provider>
+        </n-notification-provider>
+      </n-dialog-provider>
+    </n-loading-bar-provider>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import '@/styles/reset.css'
+import '@/styles/index.scss'
+import 'uno.css'
+import 'virtual:svg-icons-register'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import App from './App.vue'
+// import { setupStore } from './store'
+// import { setupRouter } from './router'
+
+const app = createApp(App)
+
+// setupStore(app)
+// setupRouter(app)
+
+app.mount('#app')
